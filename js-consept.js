8296575cb68c393847b97037b3b73f252bc98570
nandhini-kani <nandhukarthi2004@gmail.com>
2025-07-29 14:48:23 +0530

    concept-js

---
+++ js-consept.js
@@ -0,0 +1,17 @@
+var a=30;
+var b=30;
+
+var c=a+b;
+var d=a-b;
+var e=a*b;
+var f=a/b;
+var g=a%b;
+console.log("a+b=",c);
+console.log("a-b=",d);
+console.log("a*b=",e);
+console.log("a/b=",f);
+console.log("a%b=",g);
+
+
+
+
